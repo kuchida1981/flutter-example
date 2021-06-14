51cf65d137653f8745294c744b05409b17f8aef5
kuchida1981 <kuchida1981@users.noreply.github.com>
2021-06-14 14:49:44 +0000

    Automated deployment: Mon Jun 14 14:49:44 UTC 2021 5195e2a3fa3e1aa982467fabb65bffa053ec3c4e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "4ae25d36a4fde903aad66823a54e76d2",
-"/": "4ae25d36a4fde903aad66823a54e76d2",
+"index.html": "ebaaac4ff15ef5ce6bb37fd589d8b24c",
+"/": "ebaaac4ff15ef5ce6bb37fd589d8b24c",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
