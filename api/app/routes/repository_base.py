8b512a6c4d42d4b02c88b870d36b23f5e3be4cca
Kosuke Uchida <byebyeearthjpn@gmail.com>
2021-06-25 22:17:59 +0900

    Repository へ Session オブジェクトをDIで渡す

--- api/app/routes/repository_base.py
+++ api/app/routes/repository_base.py
@@ -1,7 +1,10 @@
+from fastapi import Depends
+
 from app.database import Session
+from app.dependencies import db_session
 
 
 class RepositoryBase:
 
-    def __init__(self, session: Session):
+    def __init__(self, session: Session = Depends(db_session)):
         self._session = session
--- api/app/routes/samples.py
+++ api/app/routes/samples.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from app import dependencies, models
+from app import models
 
 from .sample_repository import SampleRepository
 
@@ -15,16 +15,16 @@ router = APIRouter(
     description="Sample collection",
     response_model=models.SampleList,
 )
-async def list_item(session=Depends(dependencies.db_session)):
-    return SampleRepository(session).list_item()
+async def list_item(repository=Depends(SampleRepository)):
+    return repository.list_item()
 
 
 @router.get(
     "/{item_id}",
     response_model=models.Sample,
 )
-async def get(item_id: int, session=Depends(dependencies.db_session)):
-    if (sample := SampleRepository(session).get_item(item_id)) is None:
+async def get(item_id: int, repository=Depends(SampleRepository)):
+    if (sample := repository.get_item(item_id)) is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
 
     return sample
@@ -33,9 +33,9 @@ async def get(item_id: int, session=Depends(dependencies.db_session)):
 @router.post(
     "",
     summary="Post Sample",
-    description="Sample list",
+    description="Post Sample",
     status_code=status.HTTP_201_CREATED,
     response_model=models.Sample,
 )
-async def post(sample: models.SampleIn, session=Depends(dependencies.db_session)):
-    return SampleRepository(session).create(sample)
+async def post(sample: models.SampleIn, repository=Depends(SampleRepository)):
+    return repository.create(sample)
